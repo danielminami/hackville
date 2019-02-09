e638faffa8def363b9d00b45711f01c697f80aad
danielminami <danielminami@hotmail.com>
2019-02-09 13:54:57 -0500

    Added Retrieving users by Topic / Interest

--- src/main/java/hackville/elderly/controller/ControllerUser.java
+++ src/main/java/hackville/elderly/controller/ControllerUser.java
@@ -1,17 +1,25 @@
 package hackville.elderly.controller;
 
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import hackville.elderly.model.ModelTopicUser;
 import hackville.elderly.model.ModelUser;
 import hackville.elderly.repository.RepositoryUser;
+import hackville.elderly.resources.ResourceNotFoundException;
 
 @RestController    
 @RequestMapping(path="/user") 
@@ -41,10 +49,11 @@ public class ControllerUser {
 		return n;
 	}
 	
-	@PostMapping(path="/getById")
-	public @ResponseBody Optional<ModelUser> getById(@RequestBody ModelUser n) {
-		return repo.findById(n.getId());
+	@GetMapping(path="/getByTopic/{topicId}")
+	public @ResponseBody List<ModelUser> create(@PathVariable (value = "topicId") Integer topicId) {
+		return repo.findByTopic(topicId);
 	}
+	
 	
 }
 
--- src/main/java/hackville/elderly/repository/RepositoryUser.java
+++ src/main/java/hackville/elderly/repository/RepositoryUser.java
@@ -1,10 +1,21 @@
 package hackville.elderly.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import hackville.elderly.model.ModelUser;
 
 @Repository
 public interface RepositoryUser extends JpaRepository<ModelUser, Integer> {
+	
+	
+	@Query(value="SELECT * "
+			+ "FROM tb_user AS u, tb_topic_usr AS t "
+			+ "WHERE u.id = t.user_id and "
+			+ "t.topic_id = ?1"
+			, nativeQuery = true)
+	List<ModelUser> findByTopic(Integer topic);
 	
 }
