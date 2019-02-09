5e6ba597ebe6b2de2beefc54d101f5708ab8d95d
danielminami <danielminami@hotmail.com>
2019-02-09 09:15:02 -0500

    Added Topic Model

--- src/main/java/hackville/elderly/controller/ControllerUser.java
+++ src/main/java/hackville/elderly/controller/ControllerUser.java
@@ -1,6 +1,5 @@
 package hackville.elderly.controller;
 
-
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/hackville/elderly/model/ModelTopic.java
@@ -0,0 +1,93 @@
+package hackville.elderly.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table (name="tb_user")
+public class ModelTopic extends ModelAudit {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    
+    @NotNull
+    @Size(max = 100)
+    private String name;
+
+    @NotNull
+    @Column(unique = true)
+    @Size(max = 100)
+    private String email;
+    
+	@Size(max = 100)
+    private String location;
+	
+	@Size(max = 10)
+    private String age;
+	
+	@Size(max = 10)
+    private String gender;
+    
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}   
+	
+}
+
+
