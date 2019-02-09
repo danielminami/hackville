4941a671c81a4fe7abac2eaad3f0e292aba9a004
danielminami <danielminami@hotmail.com>
2019-02-09 00:38:49 -0500

    First commit

---
+++ src/test/java/hackville/elderly/ElderlyApplicationTests.java
@@ -0,0 +1,17 @@
+package hackville.elderly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElderlyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
