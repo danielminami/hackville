5b6a19c5920d34196401fccaf0e3e819c6d675b5
danielminami <danielminami@hotmail.com>
2019-02-09 01:40:59 -0500

    Creation of Model User

--- src/main/java/hackville/elderly/ElderlyApplication.java
+++ src/main/java/hackville/elderly/ElderlyApplication.java
@@ -2,8 +2,10 @@ package hackville.elderly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ElderlyApplication {
 
 	public static void main(String[] args) {
